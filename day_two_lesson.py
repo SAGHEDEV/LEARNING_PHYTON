f3a1d89ce65dbec5f3325f62da930471b6b2e66b
Abdul Hakeem <abdulhakeem080255@gmail.com>
2024-10-01 13:24:38 +0100

    day 2 learning phyton

---
+++ day_two_lesson.py
@@ -0,0 +1,19 @@
+#Day 2 lesson on creating and printing variables
+
+#Create variable name
+your_name = "Adekola AbdulHakeem"
+#Create variable age
+your_age = 50
+#Create variable favourite color
+favorite_color = "Blue"
+#Create variable hobby
+hobby = "Watching movies"
+
+#Print variable your_name
+print("My name is" ,your_name)
+#Print variable your_age
+print("My age is",your_age)
+#Print variable favorite_color
+print("My favorite color is",favorite_color)
+#Print variable hobby
+print("My hobby is",hobby)
