b2479e7f96405f7a2ddb769e84fff8051e0f4e7b
Abdul Hakeem <abdulhakeem080255@gmail.com>
2024-10-04 12:59:51 +0100

    day 3 learning phyton

---
+++ day_three_lesson.py
@@ -0,0 +1,14 @@
+# Creating a list called safari_gear
+safari_gear = ["Laptops","Books","Mouse","Bottle","Mat"]
+# Printing the list
+print(safari_gear)
+
+# Adding a new item to the list
+safari_gear=["Laptops","Books","Mouse","Bottle","Mat","Sunglasses"]
+# printing the new list 
+print(safari_gear)
+
+# Creating a tuple named memory_moments 
+memory_moments = ("5:30AM", "4:00PM", "Crocodile")
+# Printing the tuple 
+print(memory_moments)
