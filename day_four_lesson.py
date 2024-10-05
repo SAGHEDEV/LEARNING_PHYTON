d13af70d11bfd32752fda922e41461371cc46318
Abdul Hakeem <abdulhakeem080255@gmail.com>
2024-10-05 23:06:16 +0100

    day 4 learning phyton

---
+++ day_four_lesson.py
@@ -0,0 +1,38 @@
+# Creating variable called first_name 
+first_name = "Adekola"
+
+# Creating variable called last_name 
+last_name = "Abdulhakeem"
+
+# Printing out the name on a single line 
+print(first_name, last_name)
+
+# Creating a list called favourite_fruits
+favourite_fruits = ["Apple","Banana","Mango"]
+
+# Printing the first and the last fruit of the list 
+print(favourite_fruits[0])
+print(favourite_fruits[2])
+
+# Replacing the second fruit in the list with another
+favourite_fruits = ["Apple","Cherry","Mango"]
+
+# Printing the list 
+print(favourite_fruits)
+
+# Creating a tuple that represent coordinates
+coordinate = (20, 10, 30)
+
+# Printing the first value of the tuple
+print(coordinate[0])
+
+# Creating a dictionary called my_profile 
+my_profile = {
+    "name": "Adekola Abdulhakeem",
+    "age": 50,
+    "city": "Lagos"
+}
+
+# Printing the name and city by accessing it with the keys 
+print(my_profile["name"])
+print(my_profile["city"])
